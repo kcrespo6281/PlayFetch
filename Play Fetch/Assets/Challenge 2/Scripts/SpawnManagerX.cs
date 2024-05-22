8062768e1ac6b4e55f8555dc20ad9f3aedb6e02c
katelyn <crespok@roverkids.org>
2024-05-22 13:46:36 -0400

    Updated project

---
+++ Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int randomballIndex = Random.Range(0, ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomballIndex], spawnPos, ballPrefabs[randomballIndex].transform.rotation);
+        
+    }
+
+}
